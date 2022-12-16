6cc0416de1f504139d4966a419e3c39a6da4ec28
Issac Fei <issacfei@Issacs-MacBook-Air.local>
2022-12-16 19:47:35 +0900

    VideoRotateDone

---
+++ Temp 2/il2cppOutput/il2cppOutput/Unity.InputSystem.Recorder_CodeGen 2.c
@@ -0,0 +1,221 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Boolean UnityEngine.InputSystem.InputRecorder::get_captureIsRunning()
+extern void InputRecorder_get_captureIsRunning_mCF5FC10034A3AF6AD78B8007C3DD8C3FD38988E4 (void);
+// 0x00000002 System.Boolean UnityEngine.InputSystem.InputRecorder::get_replayIsRunning()
+extern void InputRecorder_get_replayIsRunning_mF199B8F1B382042BD22B670B27BAD078B18C2B2E (void);
+// 0x00000003 System.Boolean UnityEngine.InputSystem.InputRecorder::get_startRecordingWhenEnabled()
+extern void InputRecorder_get_startRecordingWhenEnabled_mF64E13567C452E9A8AA33A1C7266E99C407C29D7 (void);
+// 0x00000004 System.Void UnityEngine.InputSystem.InputRecorder::set_startRecordingWhenEnabled(System.Boolean)
+extern void InputRecorder_set_startRecordingWhenEnabled_m1A5619DA9E7D86B1E650CC145767D674FD34D545 (void);
+// 0x00000005 System.Int64 UnityEngine.InputSystem.InputRecorder::get_eventCount()
+extern void InputRecorder_get_eventCount_m5EAA43E30329E0F0179415B864BEE1066496085F (void);
+// 0x00000006 System.Int64 UnityEngine.InputSystem.InputRecorder::get_totalEventSizeInBytes()
+extern void InputRecorder_get_totalEventSizeInBytes_m1D984EC69676F6A79B94CABC0F55F99080427678 (void);
+// 0x00000007 System.Int64 UnityEngine.InputSystem.InputRecorder::get_allocatedSizeInBytes()
+extern void InputRecorder_get_allocatedSizeInBytes_m8DDFFE1ED473E8AA50214FFAAB46EF378149956D (void);
+// 0x00000008 System.Boolean UnityEngine.InputSystem.InputRecorder::get_recordFrames()
+extern void InputRecorder_get_recordFrames_m41FCD3089C8CBEE8D98994C10088BDF893FC9F90 (void);
+// 0x00000009 System.Void UnityEngine.InputSystem.InputRecorder::set_recordFrames(System.Boolean)
+extern void InputRecorder_set_recordFrames_m2E8AD82FDED12AEA5A1B10505CBD913EEBFADFF0 (void);
+// 0x0000000A System.Boolean UnityEngine.InputSystem.InputRecorder::get_recordStateEventsOnly()
+extern void InputRecorder_get_recordStateEventsOnly_m5093F2ED98FF8D54EBB869488CF2DD73BC3A6F44 (void);
+// 0x0000000B System.Void UnityEngine.InputSystem.InputRecorder::set_recordStateEventsOnly(System.Boolean)
+extern void InputRecorder_set_recordStateEventsOnly_mFC41D3B57FF998FF2098BC469CD3295A5365664D (void);
+// 0x0000000C System.String UnityEngine.InputSystem.InputRecorder::get_devicePath()
+extern void InputRecorder_get_devicePath_m9D758CD361E936BD96ABBAFB66F13FB4A6558C5D (void);
+// 0x0000000D System.Void UnityEngine.InputSystem.InputRecorder::set_devicePath(System.String)
+extern void InputRecorder_set_devicePath_mD8C60A2FF88068C8067CFFFC423EB4F4B8431670 (void);
+// 0x0000000E System.String UnityEngine.InputSystem.InputRecorder::get_recordButtonPath()
+extern void InputRecorder_get_recordButtonPath_m134E26B9BBE4EB360A04ECA7B7FA982D166978A6 (void);
+// 0x0000000F System.Void UnityEngine.InputSystem.InputRecorder::set_recordButtonPath(System.String)
+extern void InputRecorder_set_recordButtonPath_m18219934D2E012A7835AAAF0835494F3512BE073 (void);
+// 0x00000010 System.String UnityEngine.InputSystem.InputRecorder::get_playButtonPath()
+extern void InputRecorder_get_playButtonPath_mE7A5B9BB3EF652E52200D012E92BBFD0E918F112 (void);
+// 0x00000011 System.Void UnityEngine.InputSystem.InputRecorder::set_playButtonPath(System.String)
+extern void InputRecorder_set_playButtonPath_m39ACE6BFC946DDCCD1D95F4A3DBFB7C4E78D96E1 (void);
+// 0x00000012 UnityEngine.InputSystem.LowLevel.InputEventTrace UnityEngine.InputSystem.InputRecorder::get_capture()
+extern void InputRecorder_get_capture_m2331DCA37ADCE18EA2F5868916474B4EB9B6BAF8 (void);
+// 0x00000013 UnityEngine.InputSystem.LowLevel.InputEventTrace/ReplayController UnityEngine.InputSystem.InputRecorder::get_replay()
+extern void InputRecorder_get_replay_m0384533F04BCAFA912CBCA37E4314A56161EFBE0 (void);
+// 0x00000014 System.Int32 UnityEngine.InputSystem.InputRecorder::get_replayPosition()
+extern void InputRecorder_get_replayPosition_mD8CC76831C1163CB9CADEA08F17BBA2A34306289 (void);
+// 0x00000015 System.Boolean UnityEngine.InputSystem.InputRecorder::get_replayOnNewDevices()
+extern void InputRecorder_get_replayOnNewDevices_mE7BD604DE7081CEC46F4942290732BC287A83241 (void);
+// 0x00000016 System.Void UnityEngine.InputSystem.InputRecorder::set_replayOnNewDevices(System.Boolean)
+extern void InputRecorder_set_replayOnNewDevices_m7CECBEA1E09FBED5D497E37929D9399ABF63A960 (void);
+// 0x00000017 System.Boolean UnityEngine.InputSystem.InputRecorder::get_simulateOriginalTimingOnReplay()
+extern void InputRecorder_get_simulateOriginalTimingOnReplay_m959BF1341D2B14CB9057B2C7EF865845A117EC9F (void);
+// 0x00000018 System.Void UnityEngine.InputSystem.InputRecorder::set_simulateOriginalTimingOnReplay(System.Boolean)
+extern void InputRecorder_set_simulateOriginalTimingOnReplay_mD962ECBD46A4B94C9B4CAD6ED5A14C45C3DD8993 (void);
+// 0x00000019 UnityEngine.InputSystem.InputRecorder/ChangeEvent UnityEngine.InputSystem.InputRecorder::get_changeEvent()
+extern void InputRecorder_get_changeEvent_mAF781E435C4D51483D512B925E39FDFB645B0397 (void);
+// 0x0000001A System.Void UnityEngine.InputSystem.InputRecorder::StartCapture()
+extern void InputRecorder_StartCapture_mFD03BE3AA91353DCC524C2F46BE90D6A39A477E2 (void);
+// 0x0000001B System.Void UnityEngine.InputSystem.InputRecorder::StopCapture()
+extern void InputRecorder_StopCapture_m48FD9D3EFDBC042E8307F9BCD97BC5343184B928 (void);
+// 0x0000001C System.Void UnityEngine.InputSystem.InputRecorder::StartReplay()
+extern void InputRecorder_StartReplay_m8EFC0C0A66B52AA8774B46AA0778BA10A1FC5B7D (void);
+// 0x0000001D System.Void UnityEngine.InputSystem.InputRecorder::StopReplay()
+extern void InputRecorder_StopReplay_m9B624302F19B69BB744199FB5AD26D02F24D6C52 (void);
+// 0x0000001E System.Void UnityEngine.InputSystem.InputRecorder::PauseReplay()
+extern void InputRecorder_PauseReplay_m57A92339DA3D24B6416DE7665D98DAA9ADDC9015 (void);
+// 0x0000001F System.Void UnityEngine.InputSystem.InputRecorder::ClearCapture()
+extern void InputRecorder_ClearCapture_mE77CA70C22B594B32471295757C3321F15A84A9D (void);
+// 0x00000020 System.Void UnityEngine.InputSystem.InputRecorder::LoadCaptureFromFile(System.String)
+extern void InputRecorder_LoadCaptureFromFile_m3CD2CB9F2FF130D609175B301606A2D8F04780C6 (void);
+// 0x00000021 System.Void UnityEngine.InputSystem.InputRecorder::SaveCaptureToFile(System.String)
+extern void InputRecorder_SaveCaptureToFile_mE14DF7D48107DA2B7DDF3F675531DEAA3528F56F (void);
+// 0x00000022 System.Void UnityEngine.InputSystem.InputRecorder::OnEnable()
+extern void InputRecorder_OnEnable_m9E059DABF235E5B14465EDD365EAE0D45DB8F2D3 (void);
+// 0x00000023 System.Void UnityEngine.InputSystem.InputRecorder::OnDisable()
+extern void InputRecorder_OnDisable_mC16E1CDE6B8171B0C6DE20CBDACD8CE0D43F7117 (void);
+// 0x00000024 System.Void UnityEngine.InputSystem.InputRecorder::OnDestroy()
+extern void InputRecorder_OnDestroy_m64F10A837F8F7115DB2B62311380A416E84F1658 (void);
+// 0x00000025 System.Boolean UnityEngine.InputSystem.InputRecorder::OnFilterInputEvent(UnityEngine.InputSystem.LowLevel.InputEventPtr,UnityEngine.InputSystem.InputDevice)
+extern void InputRecorder_OnFilterInputEvent_m24566E4A412D4A272EDEA0B9CBD98E1D4887F595 (void);
+// 0x00000026 System.Void UnityEngine.InputSystem.InputRecorder::OnEventRecorded(UnityEngine.InputSystem.LowLevel.InputEventPtr)
+extern void InputRecorder_OnEventRecorded_mBC3B4C6C74527159D7E0F1BEAFDC52B9254D6483 (void);
+// 0x00000027 System.Void UnityEngine.InputSystem.InputRecorder::OnInputEvent(UnityEngine.InputSystem.LowLevel.InputEventPtr,UnityEngine.InputSystem.InputDevice)
+extern void InputRecorder_OnInputEvent_mE0B410CB20E21FC18AE51C1C954E14438BE27C9C (void);
+// 0x00000028 System.Void UnityEngine.InputSystem.InputRecorder::CreateEventTrace()
+extern void InputRecorder_CreateEventTrace_mAB0C0BEBC90EA923E304F01B8EA5C33C0E1D02D6 (void);
+// 0x00000029 System.Void UnityEngine.InputSystem.InputRecorder::HookOnInputEvent()
+extern void InputRecorder_HookOnInputEvent_mC8F035C58D16B12B921782ADF85D3158017BDB10 (void);
+// 0x0000002A System.Void UnityEngine.InputSystem.InputRecorder::UnhookOnInputEvent()
+extern void InputRecorder_UnhookOnInputEvent_m72CFF800A59BA70DF8F0D9B80CCD60ADD1B299C4 (void);
+// 0x0000002B System.Void UnityEngine.InputSystem.InputRecorder::.ctor()
+extern void InputRecorder__ctor_m51B0F23ABDC75E836FD7A8379EC3CE1B9BEB84FE (void);
+// 0x0000002C System.Void UnityEngine.InputSystem.InputRecorder::<StartReplay>b__44_0(UnityEngine.InputSystem.LowLevel.InputEventPtr)
+extern void InputRecorder_U3CStartReplayU3Eb__44_0_m04277791E63C0179C8922BB61CFB52CE541C3F12 (void);
+// 0x0000002D System.Void UnityEngine.InputSystem.InputRecorder/ChangeEvent::.ctor()
+extern void ChangeEvent__ctor_m9139990BD53310331E50BEE2A5DFCF085BF2FFC6 (void);
+static Il2CppMethodPointer s_methodPointers[45] = 
+{
+	InputRecorder_get_captureIsRunning_mCF5FC10034A3AF6AD78B8007C3DD8C3FD38988E4,
+	InputRecorder_get_replayIsRunning_mF199B8F1B382042BD22B670B27BAD078B18C2B2E,
+	InputRecorder_get_startRecordingWhenEnabled_mF64E13567C452E9A8AA33A1C7266E99C407C29D7,
+	InputRecorder_set_startRecordingWhenEnabled_m1A5619DA9E7D86B1E650CC145767D674FD34D545,
+	InputRecorder_get_eventCount_m5EAA43E30329E0F0179415B864BEE1066496085F,
+	InputRecorder_get_totalEventSizeInBytes_m1D984EC69676F6A79B94CABC0F55F99080427678,
+	InputRecorder_get_allocatedSizeInBytes_m8DDFFE1ED473E8AA50214FFAAB46EF378149956D,
+	InputRecorder_get_recordFrames_m41FCD3089C8CBEE8D98994C10088BDF893FC9F90,
+	InputRecorder_set_recordFrames_m2E8AD82FDED12AEA5A1B10505CBD913EEBFADFF0,
+	InputRecorder_get_recordStateEventsOnly_m5093F2ED98FF8D54EBB869488CF2DD73BC3A6F44,
+	InputRecorder_set_recordStateEventsOnly_mFC41D3B57FF998FF2098BC469CD3295A5365664D,
+	InputRecorder_get_devicePath_m9D758CD361E936BD96ABBAFB66F13FB4A6558C5D,
+	InputRecorder_set_devicePath_mD8C60A2FF88068C8067CFFFC423EB4F4B8431670,
+	InputRecorder_get_recordButtonPath_m134E26B9BBE4EB360A04ECA7B7FA982D166978A6,
+	InputRecorder_set_recordButtonPath_m18219934D2E012A7835AAAF0835494F3512BE073,
+	InputRecorder_get_playButtonPath_mE7A5B9BB3EF652E52200D012E92BBFD0E918F112,
+	InputRecorder_set_playButtonPath_m39ACE6BFC946DDCCD1D95F4A3DBFB7C4E78D96E1,
+	InputRecorder_get_capture_m2331DCA37ADCE18EA2F5868916474B4EB9B6BAF8,
+	InputRecorder_get_replay_m0384533F04BCAFA912CBCA37E4314A56161EFBE0,
+	InputRecorder_get_replayPosition_mD8CC76831C1163CB9CADEA08F17BBA2A34306289,
+	InputRecorder_get_replayOnNewDevices_mE7BD604DE7081CEC46F4942290732BC287A83241,
+	InputRecorder_set_replayOnNewDevices_m7CECBEA1E09FBED5D497E37929D9399ABF63A960,
+	InputRecorder_get_simulateOriginalTimingOnReplay_m959BF1341D2B14CB9057B2C7EF865845A117EC9F,
+	InputRecorder_set_simulateOriginalTimingOnReplay_mD962ECBD46A4B94C9B4CAD6ED5A14C45C3DD8993,
+	InputRecorder_get_changeEvent_mAF781E435C4D51483D512B925E39FDFB645B0397,
+	InputRecorder_StartCapture_mFD03BE3AA91353DCC524C2F46BE90D6A39A477E2,
+	InputRecorder_StopCapture_m48FD9D3EFDBC042E8307F9BCD97BC5343184B928,
+	InputRecorder_StartReplay_m8EFC0C0A66B52AA8774B46AA0778BA10A1FC5B7D,
+	InputRecorder_StopReplay_m9B624302F19B69BB744199FB5AD26D02F24D6C52,
+	InputRecorder_PauseReplay_m57A92339DA3D24B6416DE7665D98DAA9ADDC9015,
+	InputRecorder_ClearCapture_mE77CA70C22B594B32471295757C3321F15A84A9D,
+	InputRecorder_LoadCaptureFromFile_m3CD2CB9F2FF130D609175B301606A2D8F04780C6,
+	InputRecorder_SaveCaptureToFile_mE14DF7D48107DA2B7DDF3F675531DEAA3528F56F,
+	InputRecorder_OnEnable_m9E059DABF235E5B14465EDD365EAE0D45DB8F2D3,
+	InputRecorder_OnDisable_mC16E1CDE6B8171B0C6DE20CBDACD8CE0D43F7117,
+	InputRecorder_OnDestroy_m64F10A837F8F7115DB2B62311380A416E84F1658,
+	InputRecorder_OnFilterInputEvent_m24566E4A412D4A272EDEA0B9CBD98E1D4887F595,
+	InputRecorder_OnEventRecorded_mBC3B4C6C74527159D7E0F1BEAFDC52B9254D6483,
+	InputRecorder_OnInputEvent_mE0B410CB20E21FC18AE51C1C954E14438BE27C9C,
+	InputRecorder_CreateEventTrace_mAB0C0BEBC90EA923E304F01B8EA5C33C0E1D02D6,
+	InputRecorder_HookOnInputEvent_mC8F035C58D16B12B921782ADF85D3158017BDB10,
+	InputRecorder_UnhookOnInputEvent_m72CFF800A59BA70DF8F0D9B80CCD60ADD1B299C4,
+	InputRecorder__ctor_m51B0F23ABDC75E836FD7A8379EC3CE1B9BEB84FE,
+	InputRecorder_U3CStartReplayU3Eb__44_0_m04277791E63C0179C8922BB61CFB52CE541C3F12,
+	ChangeEvent__ctor_m9139990BD53310331E50BEE2A5DFCF085BF2FFC6,
+};
+static const int32_t s_InvokerIndices[45] = 
+{
+	5133,
+	5133,
+	5133,
+	4149,
+	5200,
+	5200,
+	5200,
+	5133,
+	4149,
+	5133,
+	4149,
+	5226,
+	4244,
+	5226,
+	4244,
+	5226,
+	4244,
+	5226,
+	5226,
+	5199,
+	5133,
+	4149,
+	5133,
+	4149,
+	5226,
+	5335,
+	5335,
+	5335,
+	5335,
+	5335,
+	5335,
+	4244,
+	4244,
+	5335,
+	5335,
+	5335,
+	1277,
+	4215,
+	1920,
+	5335,
+	5335,
+	5335,
+	5335,
+	4215,
+	5335,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_InputSystem_Recorder_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_InputSystem_Recorder_CodeGenModule = 
+{
+	"Unity.InputSystem.Recorder.dll",
+	45,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
